23e4d7041ca405670abf794af222f610d2e5b89c
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2022-12-13 14:54:27 +0300

    added object

--- app.js
+++ app.js
@@ -5,3 +5,26 @@ let c = "sdt";
 let d = true;
 let res = revenue + bonus;
 console.log(res);
+//function
+function getFullName(firstname, surname) {
+    return `${firstname} ${surname}`;
+}
+const getFullNameArrow = (firstname, surname) => {
+    return `${firstname} ${surname}`;
+};
+console.log(getFullNameArrow("dsf", "sadf"));
+//Объекты
+function getName(userEntity) {
+    return `${userEntity.firstname} ${userEntity.surname}`;
+}
+const user = {
+    firstname: 'Anton',
+    surname: 'Larich',
+    city: 'Moscow',
+    age: 33,
+    skills: {
+        dev: true,
+        devops: true
+    }
+};
+console.log(getName(user));
--- app.ts
+++ app.ts
@@ -5,3 +5,34 @@ let d: boolean = true;
 
 let res: number = revenue + bonus;
 console.log(res)
+
+//function
+
+function getFullName(firstname: string, surname: string): string{
+    return `${firstname} ${surname}`
+}
+
+const getFullNameArrow = (firstname: string, surname: string): string => {
+    return `${firstname} ${surname}`;
+}
+
+console.log(getFullNameArrow("dsf", "sadf"))
+
+//Объекты
+
+function getName(userEntity: {firstname: string, surname: string }): string {
+    return `${userEntity.firstname} ${userEntity.surname}`
+}
+
+const user = {
+    firstname: 'Anton',
+    surname: 'Larich',
+    city: 'Moscow',
+    age: 33,
+    skills: {
+        dev : true,
+        devops: true
+    }
+};
+
+console.log(getName(user));
