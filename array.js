4536ff7b63e2a932c707286faa2868dc466ceb72
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2022-12-14 17:43:34 +0300

    add enum

--- array.js
+++ array.js
@@ -7,3 +7,27 @@ const newSkills = skills
     .filter((s) => s !== 'DevOps')
     .map(s => s + '!');
 console.log(newSkills);
+// Tuples
+const skill1 = [1, "Drv"];
+const [id, skillName] = skill1;
+const arr = [1, "sdr", true, true];
+// Redonly  данный метод не позволяет изменять массив, только для просмотра
+const dkilll2 = [1, 'Dev'];
+//Enums
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode[StatusCode["IN_PROCESS"] = 2] = "IN_PROCESS";
+    StatusCode[StatusCode["FAILED"] = 3] = "FAILED";
+})(StatusCode || (StatusCode = {}));
+const res1 = {
+    massage: "Платеж успешен",
+    statusCode: StatusCode.SUCCESS
+};
+if (res1.statusCode === StatusCode.SUCCESS) {
+}
+function action(status) {
+}
+action(StatusCode.SUCCESS);
+action(1);
+action(2);
--- array.ts
+++ array.ts
@@ -9,3 +9,47 @@ const newSkills = skills
 .map(s => s + '!')
 
 console.log(newSkills);
+
+
+// Tuples
+
+const skill1: [number, string] = [1, "Drv"];
+
+const [id, skillName] = skill1;
+
+const arr: [number, string, ...boolean[]] = [1, "sdr", true, true]
+
+// Redonly  данный метод не позволяет изменять массив, только для просмотра
+
+const dkilll2: readonly [number, string] = [1, 'Dev'];
+
+
+//Enums
+
+enum StatusCode {
+    SUCCESS = 1,
+    IN_PROCESS = 2,
+    FAILED = 3
+}
+
+const res1 = {
+    massage: "Платеж успешен",
+    statusCode: StatusCode.SUCCESS
+}
+
+if (res1.statusCode === StatusCode.SUCCESS){
+    
+}
+
+function action(status: StatusCode){
+
+}
+
+action(StatusCode.SUCCESS);
+action(1)
+action(2)
+
+
+
+
+
